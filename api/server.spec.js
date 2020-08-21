828ee050d09e2d45347d24cf5c7bf56852085bf4
christiano39 <carneson39@gmail.com>
2020-08-21 10:20:33 -0600

    added tests for POST /login

--- api/server.spec.js
+++ api/server.spec.js
@@ -64,4 +64,45 @@ describe("server", () => {
       expect(users).toHaveLength(1);
     });
   });
+
+  describe("POST /login", () => {
+    beforeEach(async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "christian", password: "testing" });
+    });
+
+    it("should be 200 OK on successful login", async () => {
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "christian", password: "testing" });
+
+      expect(res.status).toBe(200);
+    });
+
+    it("should return the correct user on login", async () => {
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "christian", password: "testing" });
+
+      expect(res.body.user.username).toBe("christian");
+    });
+
+    it("should send a token on login", async () => {
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "christian", password: "testing" });
+
+      expect(res.body.token).toBeDefined();
+    });
+
+    it("should not login a user that isn't registered", async () => {
+      res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "dave", password: "testing" });
+
+      expect(res.body.token).not.toBeDefined();
+      expect(res.body.message).toMatch(/invalid credentials/i);
+    });
+  });
 });
