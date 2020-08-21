061daa5859870fb967f8e4e30801b4d67b0dcd1a
christiano39 <carneson39@gmail.com>
2020-08-21 09:29:29 -0600

    implemented auth routes for signup and login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model");
+const secret = require("../config/jwt_secret");
 
 router.post("/register", (req, res) => {
   const { username, password } = req.body;
@@ -6,10 +10,68 @@ router.post("/register", (req, res) => {
   if (!username || !password) {
     res.status(400).json({ message: "Please provide required credentials" });
   }
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user) {
+        res.status(409).json({ message: "Username is already taken" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+
+  const rounds = parseInt(process.env.HASH_ROUNDS) || 8;
+  const hash = bcrypt.hashSync(password, rounds);
+
+  Users.add({ username, password: hash })
+    .then((user) => {
+      const token = signToken(user);
+      res
+        .status(201)
+        .json({ message: "User created successfully", user, token });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res
+      .status(400)
+      .json({ message: "Please provide both username and password" });
+  }
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+        res
+          .status(200)
+          .json({ message: "Successfully logged in", user, token });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
+
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
